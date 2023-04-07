a61b86110a82cb664c0b480b07527e726bc091ff
Diogo L Scarmagnani <diogols@live.com>
2023-04-07 20:36:36 -0300

    scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import { TimeNow } from './TimeNow';
-import { StepLabel } from './StepLabel';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-
-        <div>
-          <h1>Hello, world!</h1>
-          <StepLabel />
-          <TimeNow />
-        </div>
-      </header>
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
