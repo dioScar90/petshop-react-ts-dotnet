0b4e2eb07515ab6f817596b353612e663069dc54
Diogo L Scarmagnani <diogols@live.com>
2023-04-07 19:41:24 -0300

    new migration

--- backend/Entities/ProductEntity.cs
+++ backend/Entities/ProductEntity.cs
@@ -8,5 +8,5 @@ public class ProductEntity
     public string Brand { get; set; }
     public string Title { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
 }
