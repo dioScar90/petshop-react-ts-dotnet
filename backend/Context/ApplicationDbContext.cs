5e837a90aadbe5e11e6bfce7cc1f9664be53dd87
Diogo L Scarmagnani <diogols@live.com>
2023-04-07 15:17:53 -0300

    entities

---
+++ backend/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using backend.Entities;
+
+namespace backend.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ProductEntity> Products { get; set; }
+}
